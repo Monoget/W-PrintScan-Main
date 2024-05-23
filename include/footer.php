423077162e63f39690c95adbc555e503342db04f
Monoget <monoget2@gmail.com>
2024-05-23 23:57:15 +0600

    update all

---
+++ include/footer.php
@@ -0,0 +1,171 @@
+<footer class="footer-area pt-100 pb-70">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6">
+                <div class="footer-widget">
+                    <div class="footer-logo">
+                        <a href="index.html">
+                            <img src="assets/images/logos/logo-1.png" class="footer-logo1" alt="Images">
+                            <img src="assets/images/logos/logo-2.png" class="footer-logo2" alt="Images">
+                        </a>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet, conse tempor invidunt ut labore et dolore eos et accusam et justo.
+                    </p>
+                    <ul class="social-footer-link">
+                        <li>
+                            <a href="https://www.facebook.com/" target="_blank">
+                                <i class="bx bxl-facebook"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/" target="_blank">
+                                <i class="bx bxl-linkedin"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/" target="_blank">
+                                <i class="bx bxl-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/" target="_blank">
+                                <i class="bx bxl-instagram"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-6">
+                <div class="footer-widget ps-2">
+                    <h3>Quick Links</h3>
+                    <ul class="footer-list">
+                        <li>
+                            <a href="index.html" target="_blank">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                FAQ
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                About US
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                Contact Us
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-6">
+                <div class="footer-widget ps-2">
+                    <h3>Services</h3>
+                    <ul class="footer-list">
+                        <li>
+                            <a href="#" target="_blank">
+                                Fingerprint Cards
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                FBI Fingerprinting
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                Mobile Fingerprinting
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                Fingerprint Card Conversions
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-6">
+                <div class="footer-widget ps-2">
+                    <h3>More</h3>
+                    <ul class="footer-list">
+                        <li>
+                            <a href="#" target="_blank">
+                                Terms & Condition
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                Privacy Policy
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-12">
+                <div class="footer-widget ps-2">
+                    <h3>Gallery</h3>
+                    <ul class="footer-gallery">
+                        <li>
+                            <a href="assets/images/footer-gallery/footer-gallery1.jpg">
+                                <img src="assets/images/footer-gallery/footer-gallery1.jpg" alt="image">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="assets/images/footer-gallery/footer-gallery2.jpg">
+                                <img src="assets/images/footer-gallery/footer-gallery2.jpg" alt="image">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="assets/images/footer-gallery/footer-gallery3.jpg">
+                                <img src="assets/images/footer-gallery/footer-gallery3.jpg" alt="image">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="assets/images/footer-gallery/footer-gallery4.jpg">
+                                <img src="assets/images/footer-gallery/footer-gallery4.jpg" alt="image">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="assets/images/footer-gallery/footer-gallery5.jpg">
+                                <img src="assets/images/footer-gallery/footer-gallery5.jpg" alt="image">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="assets/images/footer-gallery/footer-gallery6.jpg">
+                                <img src="assets/images/footer-gallery/footer-gallery6.jpg" alt="image">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+<div class="copy-right-area copy-right-bg">
+    <div class="container">
+        <div class="copy-right-text">
+            <p>
+                Copyright ©
+                <script>document.write(new Date().getFullYear())</script>
+                SK Fingerprint. Design and Develop by
+                <a href="https://monoget.com.bd/" target="_blank">Monoget Saha</a>
+            </p>
+        </div>
+    </div>
+</div>
+
+
+<div class="switch-box">
+    <label id="switch" class="switch">
+        <input type="checkbox" onchange="toggleTheme()" id="slider">
+        <span class="slider round"></span>
+    </label>
+</div>
---
+++ include/header.php
@@ -0,0 +1,150 @@
+<div class="preloader">
+    <div class="d-table">
+        <div class="d-table-cell">
+            <div class="spinner">
+                <div class="circle1"></div>
+                <div class="circle2"></div>
+                <div class="circle3"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<header class="top-header top-header-bg">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-9">
+                <div class="header-left">
+                    <div class="header-left-card">
+                        <ul>
+                            <li>
+                                <i class="bx bx-time"></i>
+                                SUN - THU
+                                <span>8:00 AM - 9:00 PM</span>
+                            </li>
+                            <li>
+                                <i class="bx bx-map"></i>
+                                OFFICE
+                                <span>3460 Grove Avenue</span>
+                            </li>
+                            <li>
+                                <i class="bx bx-phone-call"></i>
+                                CALL
+                                <span><a href="tel:+(103)2223333">+ (103) 222 3333</a></span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="top-right">
+                    <div class="language-list">
+                        <select class="language-list-item">
+                            <option>English</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+<div class="navbar-area">
+
+    <div class="mobile-nav">
+        <a href="home" class="logo">
+            <img src="assets/images/logos/logo-1.png" class="logo-one" alt="Logo">
+            <img src="assets/images/logos/logo-2.png" class="logo-two" alt="Logo">
+        </a>
+    </div>
+
+    <div class="main-nav main-nav-bottom">
+        <div class="container">
+            <nav class="navbar navbar-expand-md navbar-light ">
+                <a class="navbar-brand" href="home">
+                    <img src="assets/images/logos/logo-1.png" class="logo-one" alt="Logo">
+                    <img src="assets/images/logos/logo-2.png" class="logo-two" alt="Logo">
+                </a>
+                <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+                    <ul class="navbar-nav m-auto">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link active">
+                                Home
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                Services
+                                <i class="bx bx-chevron-down"></i>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        Fingerprint Cards
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        FBI Fingerprinting
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        Mobile Fingerprinting
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        Fingerprint Card Conversions
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                About Us
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                FAQ
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                Contact Us
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="nav-bar-side">
+                        <div class="nav-btn">
+                            <a href="#" class="default-btn-two">Get Started</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <div class="side-nav-responsive">
+        <div class="container-max">
+            <div class="dot-menu">
+                <div class="circle-inner">
+                    <div class="circle circle-one"></div>
+                    <div class="circle circle-two"></div>
+                    <div class="circle circle-three"></div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="side-nav-inner">
+                    <div class="side-nav justify-content-center align-items-center">
+                        <div class="side-nav-item">
+                            <a href="#" class="side-nav-btn">Get Started</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
